690f5f83208fd6cf4ccf390510630092fa09e79a
MUKUL SAINI <mukulsaini1843@gmail.com>
2026-04-04 23:47:40 +0530

    fixes

--- SmartContactManager/src/main/java/com/smart/SmartContactManagerApplication.java
+++ SmartContactManager/src/main/java/com/smart/SmartContactManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SmartContentMangerApplication {
+public class SmartContactManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SmartContentMangerApplication.class, args);
+		SpringApplication.run(SmartContactManagerApplication.class, args);
 	}
 
 }
