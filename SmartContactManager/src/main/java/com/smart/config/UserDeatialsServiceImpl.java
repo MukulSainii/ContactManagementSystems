df3e154bbd72c0f4151c794381d7344f3ede2026
MUKUL SAINI <mukulsaini1843@gmail.com>
2026-04-04 23:05:48 +0530

    rename project

---
+++ SmartContactManager/src/main/java/com/smart/config/UserDeatialsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.smart.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+
+public class UserDeatialsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Loads user details by username
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Fetching user from the database
+        User user = userRepository.getUserByUserName(username)
+                .orElseThrow(()-> new RuntimeException("User Not Found"));
+        if (user == null) {
+            throw new UsernameNotFoundException("Could not find user!!");
+        }
+        CustomUserDetails customUserDetails = new CustomUserDetails(user);
+        return customUserDetails;
+    }
+}
