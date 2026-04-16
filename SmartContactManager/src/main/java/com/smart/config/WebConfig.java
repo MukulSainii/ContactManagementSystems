de56b36e3122797dfe2d6001d90f64754b3e0bb4
Mukul saini <161330430+MukulSainii@users.noreply.github.com>
2026-04-16 12:06:25 +0530

    Update WebConfig.java

--- SmartContactManager/src/main/java/com/smart/config/WebConfig.java
+++ SmartContactManager/src/main/java/com/smart/config/WebConfig.java
@@ -5,6 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Spring Boot doesnot know how to access folder outside the project.
+ * So, it’s a resource mapping between a URL path(project path image store) and a file system location.
+ * It is called Static Resource Mapping (Resource Handler Mapping).
+ * serve static files (like uploaded images) from your file system through a URL in your Spring Boot application.
+ * /Image/**  →  file:<uploadDir>/ 
+ * when request come like http://localhost:8080/Image/test.jpg convert to <uploadDir>/test.jpg(C:/uploads/photo.jpg)
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Value("${file.upload-dir}")
