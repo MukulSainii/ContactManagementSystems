f376d1c732ced07144f3212bad1fdfdb4cb14ccb
Mukul saini <161330430+MukulSainii@users.noreply.github.com>
2026-04-18 15:54:44 +0530

    Add comments to GlobalBindingConfig for clarity
    Added comments explaining the purpose and functionality of the GlobalBindingConfig class and its initBinder method.

--- SmartContactManager/src/main/java/com/smart/config/GlobalBindingConfig.java
+++ SmartContactManager/src/main/java/com/smart/config/GlobalBindingConfig.java
@@ -5,6 +5,21 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.InitBinder;
 
+/*
+*this advice run before every request
+*@InitBinder = By default Spring automatically binds incoming request data (like form fields, query params, JSON, etc.) to Java objects binder.But Now this annotation allow you to handle Custom Binding 
+* WebDataBinder  = the incoming data will store in this object. You can customizing how it handles certain types (here: String).
+*registerCustomEditor() = this method specify that the incoming String data should be Trim whitespace from both side and convert empty string to null
+* Without this:  Empty strings ("") are treated as valid values and store in DB, even if you use @NotNull on field, because not null only prevent null value should't be Store, but it allow empty string
+* for prevention empty string we can use @NotBlank with @NotBlank but still if user add whitespace before and after the string, it will save as it as.
+* so registerCustomEditor() method senitize the string (It do two things 1.Trims whiteSpace, 2.Convert empty String to null)
+*example: Trims whitespace
+                  " hello " → "hello"
+        Converts empty strings to null
+                  "" → null
+                  " " → null (because trimmed)
+*/
+
 @ControllerAdvice
 public class GlobalBindingConfig {
     @InitBinder
